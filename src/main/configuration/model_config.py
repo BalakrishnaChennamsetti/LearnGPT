14539037e9f76d2ba507fbbf886c3538935b5b2d
Balakrishna Chennamsetti <balakrishna.chennamsetti@gmail.com>
2025-06-08 16:47:42 +0530

    fix: Prepared Data Classes and Prepared the Test and Validation Data Splits

--- src/main/configuration/model_config.py
+++ src/main/configuration/model_config.py
@@ -2,6 +2,16 @@ from dataclasses import dataclass
 
 @dataclass
 class ModelConfig:
-    contex_size: int = 128
-    embedding_size: int = 1024
-    window_size: int = 8
+    batch_size: int = 16
+    max_len: int = 128
+    stride: int  = 64
+    vocab_size: int = 50257   # Vocabulary size
+    context_length: int = 128 # Shortened context length (orig: 1024)
+    emb_dim: int = 768        # Embedding dimension
+    n_heads: int  = 12        # Number of attention heads
+    n_layers: int = 12        # Number of layers
+    drop_rate: int = 0.1 
+    shuffle: bool = True     # Dropout rate
+    qkv_bias: bool = False 
+    drop_last: bool = True 
+    num_workers: int = 0
